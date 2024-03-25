460f922451098f1b8cc47b923ef9e534fd21aa23
Sainath <sainathdora4@gmail.com>
2024-03-25 13:10:18 +0530

    added typewriter effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import NavBar from "@/Components/NavBar";
 import Image from "next/image";
 import BackgroundImg from "../../public/assets/Background3.svg";
+import Hero from "@/Components/Hero";
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
         className="w-full absolute -z-10"
       />
       <NavBar />
+      <Hero />
     </>
   );
 }
