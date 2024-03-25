ced12ae4b39edbaed931817fc81330391acd99ee
Sainath <sainathdora4@gmail.com>
2024-03-25 21:56:56 +0530

    working in mobile as well responsive design

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,25 @@
 export default function NavBar() {
   return (
     <>
-      <nav className="z-2 bg-transparent bg-slate-800 text-white flex">
+      <nav className="z-2 bg-transparent bg-slate-800 text-white flex sm:text-xl lg:text-3xl">
         <h2
           style={{
             width: "50%",
           }}
+          className="md:text-center font-bold sm:text-left"
         >
           InvestIQ
         </h2>
         <ul
-          className="flex justify-around"
+          className="flex justify-evenly text-xl md:text-2xl"
           style={{
             width: "50%",
           }}
         >
-          <li>Home</li>
-          <li>About</li>
-          <li>Get Started</li>
+          <li className="hover:text-gray-300 hover:cursor-pointer">Home</li>
+          <li className="hover:text-gray-300 hover:cursor-pointer">
+            Get Started
+          </li>
         </ul>
       </nav>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,13 @@ import Footer from "@/Components/Footer";
 export default function Home() {
   return (
     <>
-      <Image
-        src={BackgroundImg}
-        height={1000}
-        width={1000}
-        className="w-full absolute -z-10"
-      />
-      <NavBar />
-      <Hero />
-      <Footer />
-      <article>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum modi
-        ipsum tenetur eaque est omnis. Suscipit cumque dolorum dolor eos sed
-        placeat. Velit dolorum optio ad numquam nostrum tempore officia eligendi
-        harum at hic sequi magni dolorem, quaerat quidem ex architecto fugiat
-        adipisci quasi fugit exercitationem accusamus nam sit. Porro velit
-        libero distinctio quibusdam sit alias incidunt repellendus modi, fugit
-        deleniti unde nulla ea esse numquam temporibus impedit inventore at.
-        Illo cum labore maiores blanditiis? Cupiditate est amet nemo blanditiis
-        explicabo maiores doloribus deserunt. Natus totam quia et obcaecati
-        blanditiis facere eos placeat reprehenderit deleniti ad quae, quod, nemo
-        quam.
-      </article>
+      <div
+        className="container min-w-screen sm:min-h-screen bg-[url('../../public/assets/Background3.svg')] bg-center
+      bg-cover"
+      >
+        <NavBar />
+        <Hero />
+      </div>
     </>
   );
 }
