00f9613ea6966adb9452b091b6a4ddd81bece794
Sainath <sainathdora4@gmail.com>
2024-05-25 16:53:20 +0530

    added navigation

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -40,7 +40,7 @@ export default function Hero() {
               </svg>
             </a>
             <a
-              href="#"
+              href="/portfolio"
               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               Get Started
